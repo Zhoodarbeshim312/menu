a47d11242271c7976a00e99a8feae5fd00dbd1f6
Joodarbeshim05 <162398776+Joodarbeshim05@users.noreply.github.com>
2025-09-11 12:48:51 +0600

    End

--- src/components/pages/menu/Menu.jsx
+++ src/components/pages/menu/Menu.jsx
@@ -1,73 +1,35 @@
-import React, { useState } from "react"; // Added React import
+import React, { useState, useEffect } from "react";
 import scss from "./Menu.module.scss";
 import { useChangeLanguage } from "../../store/useLanguage";
 import axios from "axios";
-import { useEffect } from "react";
-// import menuProduct from "../../../assets/images/menuProduct.svg";
-// import { IoClose } from "react-icons/io5";
 
 const Menu = () => {
   const [selectedProduct, setSelectedProduct] = useState(null);
 
-  // const products = [
-  //   {
-  //     id: 1,
-  //     img: "https://images.unsplash.com/photo-1563805042-7684c019e1cb?w=400&h=300&fit=crop",
-  //     name: "Ice Cream",
-  //     price: 9.11,
-  //     description: "soda,cream,milk,sugar",
-  //   },
-  //   {
-  //     id: 2,
-  //     img: "https://images.unsplash.com/photo-1578985545062-69928b1d9587?w=400&h=300&fit=crop",
-  //     name: "Chocolate Dessert",
-  //     price: 12.5,
-  //     description: "cocoa,butter,cream,sugar",
-  //   },
-  //   {
-  //     id: 3,
-  //     img: "https://images.unsplash.com/photo-1572490122747-3968b75cc699?w=400&h=300&fit=crop",
-  //     name: "Vanilla Shake",
-  //     price: 8.3,
-  //     description: "milk,vanilla,sugar",
-  //   },
-  //   {
-  //     id: 4,
-  //     img: "https://images.unsplash.com/photo-1586985289906-406988974504?w=400&h=300&fit=crop",
-  //     name: "Red Velvet",
-  //     price: 10.9,
-  //     description: "cream,red cake,sugar",
-  //   },
-  // ];
-
-  const categories = [
-    "Desserts",
-    "Hot Drinks",
-    "Cold Drinks",
-    "National Foods",
-    "Eastern cuisine",
-    "Fast foods",
-  ];
-
-  const extras = [
-    { name: "Cherry", price: 0.9 },
-    { name: "Chocolate", price: 0.9 },
-  ];
-
-  const drinks = [
-    { name: "Coca Cola", price: 0.9 },
-    { name: "Coca Cola", price: 0.9 },
-  ];
   const { lang } = useChangeLanguage();
   const [product, setProduct] = useState([]);
+  const [filteredProducts, setFilteredProducts] = useState([]);
+
   const getProduct = async () => {
     let res = await axios.get(`http://54.152.44.106/${lang}/products/`);
     setProduct(res.data);
-    console.log(res.data);
+    setFilteredProducts(res.data);
   };
+
   useEffect(() => {
     getProduct();
-  }, []);
+  }, [lang]); 
+
+  const filterProducts = (category) => {
+    if (!category) {
+      setFilteredProducts(product); 
+    } else {
+      const filtered = product.filter((el) => el.category === category);
+      setFilteredProducts(filtered);
+    }
+    setSelectedProduct(null); 
+  };
+
   return (
     <section id={scss.Menu}>
       <div className="container">
@@ -78,129 +40,141 @@ const Menu = () => {
                 className={scss.closeBtn}
                 onClick={() => setSelectedProduct(null)}
               >
-                {/* <IoClose size={22} /> */}×
+                ✕
               </button>
             )}
 
             <div className={scss.btns}>
-              <button className={scss.btn}>
+              <button
+                onClick={() => filterProducts("Desserts")}
+                className={scss.btn}
+              >
                 Desserts <div className={scss.line}></div>
               </button>
 
-              <button className={scss.btn}>
+              <button
+                onClick={() => filterProducts("Cold Drinks")}
+                className={scss.btn}
+              >
                 Cold Drinks <div className={scss.line}></div>
               </button>
 
-              <button className={scss.btn}>
+              <button
+                onClick={() => filterProducts("Hot Drinks")}
+                className={scss.btn}
+              >
                 Hot Drinks <div className={scss.line}></div>
               </button>
 
-              <button className={scss.btn}>
+              <button
+                onClick={() => filterProducts("National Foods")}
+                className={scss.btn}
+              >
                 National Foods <div className={scss.line}></div>
               </button>
 
-              <button className={scss.btn}>
+              <button
+                onClick={() => filterProducts("Eastern cuisine")}
+                className={scss.btn}
+              >
                 Eastern cuisine <div className={scss.line}></div>
               </button>
 
-              <button className={scss.btn}>
+              <button
+                onClick={() => filterProducts("Fast foods")}
+                className={scss.btn}
+              >
                 Fast foods <div className={scss.line}></div>
+              </button>
+
+              <button onClick={() => filterProducts("")} className={scss.btn}>
+                All <div className={scss.line}></div>
               </button>
             </div>
           </div>
 
-          {product.map((el) => (
-            <div key={el.id} className={scss.content}>
-              {selectedProduct ? (
-                <>
-                  <div className={scss.details}>
-                    <div className={scss.info}>
-                      <img
-                        src={el.images_product.map((el) => el.image)}
-                        alt={el.name}
-                      />
-                      <div className={scss.info_bottom}>
-                        <div className={scss.info_right}>
-                          <h2>{selectedProduct.name}</h2>
-                          <p>{selectedProduct.description}</p>
-                        </div>
-                        <span>${selectedProduct.price}</span>
+          <div className={scss.content}>
+            {selectedProduct ? (
+              <>
+                <div className={scss.details}>
+                  <div className={scss.info}>
+                    <img
+                      src={selectedProduct.images_product[0]?.image}
+                      alt={selectedProduct.name}
+                    />
+                    <div className={scss.info_bottom}>
+                      <div className={scss.info_right}>
+                        <h2>{selectedProduct.name}</h2>
+                        <p>{selectedProduct.description}</p>
+                      </div>
+                      <span>${selectedProduct.price}</span>
+                    </div>
+                  </div>
+                  <div className={scss.extras}>
+                    <div className={scss.drinks}>
+                      <h4>Extras</h4>
+                      <div className={scss.price}>
+                        <p>Cherry</p>
+                        <p>$0.90</p>
+                      </div>
+                      <div className={scss.price}>
+                        <p>Cherry</p>
+                        <p>$0.90</p>
                       </div>
                     </div>
-                    <div className={scss.extras}>
-                      <div className={scss.drinks}>
-                        <h4>Extras</h4>
-                        <div className={scss.price}>
-                          <p>Cherry</p>
-                          <p>$0.90</p>
-                        </div>
-                        <div className={scss.price}>
-                          <p>Cherry</p>
-                          <p>$0.90</p>
-                        </div>
+                    <div className={scss.drinks}>
+                      <h4>Drinks</h4>
+                      <div className={scss.price}>
+                        <p>Cola</p>
+                        <p>$0.90</p>
                       </div>
-                      <div className={scss.drinks}>
-                        <h4>Drinks</h4>
-                        <div className={scss.price}>
-                          <p>Cola</p>
-                          <p>$0.90</p>
-                        </div>
-                        <div className={scss.price}>
-                          <p>Cola</p>
-                          <p>$0.90</p>
-                        </div>
+                      <div className={scss.price}>
+                        <p>Cola</p>
+                        <p>$0.90</p>
                       </div>
                     </div>
                   </div>
-
-                  {/* Similar queries */}
-                  <div className={scss.similar}>
-                    <h3>Similar queries</h3>
-                    <div className={scss.cards}>
-                      {product.map((el, idx) => (
-                        <div
-                          key={el.id}
-                          className={scss.card}
-                          onClick={() => setSelectedProduct(el)}
-                        >
-                          <img
-                            src={el.images_product.map((el) => el.image)}
-                            alt={el.name}
-                          />
-                          <div className={scss.cardInfo}>
-                            <h4>{el.name}</h4>
-                            <p>{el.description}</p>
-                            <span>${el.price}</span>
-                          </div>
-                        </div>
-                      ))}
-                    </div>
-                  </div>
-                </>
-              ) : (
-                /* Если продукт не выбран */
-                <div className={scss.products}>
-                  {product.map((el, idx) => (
-                    <div
-                      key={el.id}
-                      className={scss.card}
-                      onClick={() => setSelectedProduct(el)}
-                    >
-                      <img
-                        src={el.images_product.map((el) => el.image)}
-                        alt={el.name}
-                      />
-                      <div className={scss.cardInfo}>
-                        <h4>{el.title}</h4>
-                        <p>{el.description}</p>
-                        <span>${el.price}</span>
-                      </div>
-                    </div>
-                  ))}
                 </div>
-              )}
-            </div>
-          ))}
+
+                <div className={scss.similar}>
+                  <h3>Similar queries</h3>
+                  <div className={scss.cards}>
+                    {filteredProducts.map((el) => (
+                      <div
+                        key={el.id}
+                        className={scss.card}
+                        onClick={() => setSelectedProduct(el)}
+                      >
+                        <img src={el.images_product[0]?.image} alt={el.name} />
+                        <div className={scss.cardInfo}>
+                          <h4>{el.title}</h4>
+                          <p>{el.description}</p>
+                          <span>${el.price}</span>
+                        </div>
+                      </div>
+                    ))}
+                  </div>
+                </div>
+              </>
+            ) : (
+              <div className={scss.products}>
+                {filteredProducts.map((el) => (
+                  <div
+                    key={el.id}
+                    className={scss.card}
+                    onClick={() => setSelectedProduct(el)}
+                  >
+                    <img src={el.images_product[0]?.image} alt={el.name} />
+                    <div className={scss.cardInfo}>
+                      <h4>{el.title}</h4>
+                      <p>{el.description}</p>
+                      <span>${el.price}</span>
+                    </div>
+                  </div>
+                ))}
+              </div>
+            )}
+          </div>
         </div>
       </div>
     </section>
